a4151e1674e8e47d65722288f7821b05d1122e67
shouting crow <jinwook5062@gmail.com>
2023-09-23 15:23:22 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        for (int n : array){
+            while (n % 10 > 0){
+                if (n % 10 == 7) answer++;
+                n /= 10;
+            }
+        }
+        return answer;
+    }
+}
