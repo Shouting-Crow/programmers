d09a798ab76e23148b5c29ffb8818de9275d7d63
shouting crow <jinwook5062@gmail.com>
2023-10-27 09:18:49 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,10 +5,9 @@ class Solution {
         int[] answer = new int[commands.length];
 
         for (int i = 0; i < commands.length; i++) {
-            int startIndex = commands[i][0]-1;
-            int len = commands[i][1] - commands[i][0] + 1;
 
-            int[] arr = Arrays.stream(array).skip(startIndex).limit(len).sorted().toArray();
+            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(arr);
 
             answer[i] = arr[commands[i][2]-1];
 
