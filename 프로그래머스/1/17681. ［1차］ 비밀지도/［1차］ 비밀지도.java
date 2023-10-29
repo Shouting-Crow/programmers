a95d400828f9abccd62df099e89a6fe961567773
shouting crow <jinwook5062@gmail.com>
2023-10-29 10:12:21 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.48 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        int[] resultMap = new int[n];
+
+        for (int i = 0; i < n; i++) {
+             resultMap[i] = arr1[i] | arr2[i];
+        }
+
+        System.out.println(Arrays.toString(resultMap));
+
+        for (int i = 0; i < resultMap.length; i++) {
+            answer[i] = Integer.toBinaryString(resultMap[i]);
+
+            while (answer[i].length() < n){
+                String str = "0";
+                answer[i] = str.concat(answer[i]);
+            }
+
+            answer[i] = answer[i].replaceAll("1", "#").replaceAll("0", " ");
+        }
+
+        return answer;
+    }
+}
